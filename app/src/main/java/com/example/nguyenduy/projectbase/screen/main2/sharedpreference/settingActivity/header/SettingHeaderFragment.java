3a61682ed1e72febdea17d26c15fcbe115386f38
Nguyen Ngoc Duy <duynn03@gmail.com>
2018-08-24 18:19:58 +0700

    24-08

--- app/src/main/java/com/example/nguyenduy/projectbase/screen/main2/sharedpreference/settingActivity/header/SettingHeaderFragment.java
+++ app/src/main/java/com/example/nguyenduy/projectbase/screen/main2/sharedpreference/settingActivity/header/SettingHeaderFragment.java
@@ -1,13 +1,13 @@
 package com.example.nguyenduy.projectbase.screen.main2.sharedpreference.settingActivity.header;
 
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 
 import com.example.nguyenduy.projectbase.R;
+import com.example.nguyenduy.projectbase.screen.main2.sharedpreference.settingActivity.SettingsActivity;
 import com.example.nguyenduy.projectbase.utils.method.ResourceUtils;
 
-public class SettingHeaderFragment extends PreferenceFragment {
+public class SettingHeaderFragment extends BasePreferenceFragment {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,5 +18,16 @@ public class SettingHeaderFragment extends PreferenceFragment {
         } else if (ResourceUtils.getString(R.string.pref_extra_value_header_2).equals(key)) {
             addPreferencesFromResource(R.xml.preference_activity_header_fragment_2);
         }
+    }
+
+    @Override
+    public String getTitleActionBar() {
+        String key = getArguments().getString(ResourceUtils.getString(R.string.pref_extra_key));
+        if (ResourceUtils.getString(R.string.pref_extra_value_header_1).equals(key)) {
+            return ResourceUtils.getString(R.string.pref_header_fragment_1_title);
+        } else if (ResourceUtils.getString(R.string.pref_extra_value_header_2).equals(key)) {
+            return ResourceUtils.getString(R.string.pref_header_fragment_2_title);
+        }
+        return null;
     }
 }
